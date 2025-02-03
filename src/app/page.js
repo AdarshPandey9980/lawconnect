8e6d404040f4ddf7d4fe68f6aedf4a81431630b4
AdarshPandey9980 <stud9980y@gmail.com>
2025-02-03 22:41:13 +0530

    fixed error

--- src/app/page.js
+++ src/app/page.js
@@ -2,9 +2,11 @@ import Header from "@/components/header";
 import Hero from "@/components/hero";
 import Services from "@/components/services";
 import Whychoose from "@/components/whychoose";
+// import Footer from "@/components/footer";
 import Footer from "@/components/footer";
 import TestimonialSection from "@/components/testimonial";
-import About from "@/components/about";
+import About from "@/components/about"
+// import About from "@/components/about";
 import Lower from "@/components/lower-footer";
 export default function Main() {
   return (
